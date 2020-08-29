42bf9cfb0f48291dbb28fa612f5e600f18b7eb88
Altemush Bhatti <altemush@gmail.com>
2020-08-28 19:22:06 -0700

    Installed react and started working on components

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => (
+    <div className="App">
+      test
+    </div>
+  );
+
+
+export default App;
