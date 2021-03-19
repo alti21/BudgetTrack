4a0b243818bb306679937224c86cc82a2487813d
Altemush Bhatti <altemush@gmail.com>
2021-03-19 16:04:07 -0700

    Updates

---
+++ precache-manifest.677b97859537a581b55cfb8b442e0db3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bef73d8bd09df5fa8573357cd379c361",
+    "url": "/More-React-Practice/index.html"
+  },
+  {
+    "revision": "4267d7f42283ae564ebe",
+    "url": "/More-React-Practice/static/css/main.c333ac21.chunk.css"
+  },
+  {
+    "revision": "beb296a18157ba8dc089",
+    "url": "/More-React-Practice/static/js/2.939a358a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/More-React-Practice/static/js/2.939a358a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4267d7f42283ae564ebe",
+    "url": "/More-React-Practice/static/js/main.9d27c6e2.chunk.js"
+  },
+  {
+    "revision": "9ff79de2cb2c318fa570",
+    "url": "/More-React-Practice/static/js/runtime-main.655baf77.js"
+  }
+]);
